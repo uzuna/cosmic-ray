fd463d1d8c3d1adfad7cdb915694bfc76a1e6c5e
Fumiya Fujinaka <uzuna.kf@gmail.com>
2023-01-19 22:18:29 +0900

    fix: test PyBytes

--- cosmic-ray-py/src/lib.rs
+++ cosmic-ray-py/src/lib.rs
@@ -1,13 +1,64 @@
-use pyo3::prelude::*;
+use pyo3::{exceptions::PyValueError, prelude::*, types::PyBytes};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass]
+struct Number(i32);
+
+#[pymethods]
+impl Number {
+    #[new]
+    fn py_new(value: i32) -> PyResult<Self> {
+        if value == 0 {
+            Err(PyValueError::new_err("cannot be zero"))
+        } else {
+            Ok(Number(value))
+        }
+    }
+    fn add(&mut self, rhs: i32) -> PyResult<i32> {
+        self.0 += rhs;
+        Ok(self.0)
+    }
+    fn get(&self) -> PyResult<i32> {
+        Ok(self.0)
+    }
 }
 
+#[pyclass]
+struct ByteBox {
+    #[pyo3(get)]
+    elements: Vec<u8>,
+}
+
+#[pymethods]
+impl ByteBox {
+    #[new]
+    fn py_new(value: &PyBytes) -> PyResult<Self> {
+        let elements = value.as_bytes().to_vec();
+        Ok(Self { elements })
+    }
+}
+
+// impl FromPyObject<'_> for ByteBox {
+//     fn extract(ob: &PyAny) -> PyResult<Self> {
+//         let byte: &PyBytes = ob.downcast()?;
+//         let elements = byte.as_bytes().to_vec();
+//         Ok(Self{elements})
+//     }
+// }
+
+// #[pyproto]
+// impl PySequenceProtocol for ByteBox {
+//     fn __concat__(&self, other: PyRef<'p, Self>) -> PyResult<Self> {
+//         let mut elements = self.elements.clone();
+//         elements.extend_from_slice(&other.elements);
+//         Ok(Self { elements })
+//     }
+// }
+
+// moduleの実装定義
+// 関数名はmoduke名にするか #[pyo3(name = "custom_name")] で指定する
 #[pymodule]
 fn cosmic_ray(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Number>()?;
+    m.add_class::<ByteBox>()?;
     Ok(())
 }
---
+++ cosmic-ray-py/test/test_number.py
@@ -0,0 +1,13 @@
+import pytest
+
+from cosmic_ray import Number, ByteBox
+
+def test_number():
+    n = Number(21)
+    n.add(45)
+    assert n.get() == 66
+
+def test_bytebox():
+    b = ByteBox(b'\x7f\x45\x4c\x46\x01\x01\x01\x00')
+    print(b.elements)
+
